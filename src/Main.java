49f0c4abf03e57407da1fb39092f480e83185c3d
Diego Vanegar Zuniga <diego.vanegaszuniga@ad.snow.edu>
2019-03-07 20:42:20 -0700

    Testing

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,39 @@ public class Main {
                 break;
             }
         }
+
+        System.out.printf(
+                "Choose sandwich:\n\t" +
+                        "1. Burger\n\t" +
+                        "2. Sub\n\n" +
+                        "[Your choice] :");
+
+        Scanner cin = new Scanner(System.in);
+        int choice = cin.nextInt();
+        switch (choice){
+            case 1:{
+                Sandwich burger = new Hamburger();
+                Vector<String> condiments = new Vector<>();
+                condiments.addAll(Arrays.asList(new String[]{"Salt", "Pepper", "Oregano"}));
+                Vector<String> sauces = new Vector<>();
+                sauces.addAll(Arrays.asList(new String[]{"Ketchup", "Siracha", "Chipotle"}));
+                burger.prepareSandwich("Sesame Bum", "Burger Patty", "American", condiments, sauces, true);
+                System.out.println(burger.getDescription());
+                break;
+            }
+            case 2:{
+                Sandwich sub = new Sub();
+                Vector<String> condiments = new Vector<>();
+                condiments.addAll(Arrays.asList(new String[]{"Salt", "Pepper", "Oregano"}));
+                Vector<String> sauces = new Vector<>();
+                sauces.addAll(Arrays.asList(new String[]{"Ketchup", "Siracha", "Chipotle"}));
+                sub.prepareSandwich("Italian Herbs and Cheese", "Ham", "American", condiments, sauces, false);
+                System.out.println(sub.getDescription());
+                break;
+            }
+        }
     }
 
 
-    //TODO: Create a menu
 
 }
